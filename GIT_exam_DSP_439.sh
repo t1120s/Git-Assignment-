4e2f7216c4150482d14aea755eb9847aa3d0f0cc
Rafa <rvillanueva@uri.edu>
2023-04-13 15:44:26 -0400

    Second challenge first line change

--- GIT_exam_DSP_439.sh
+++ GIT_exam_DSP_439.sh
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-Hello, this is a change for data exam 3.
+Hello, this is a change for our class DSP 439.
 =======
 Hello, my name is Rafael, this is a  trial!!!!
 >>>>>>> ff49b5de164e28840f08d6c716deafeb674c5332
