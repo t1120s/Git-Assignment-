86f46b48b5ae7e80dc7bc7fbf868b20f1aeafe03
t1120s <129977907+t1120s@users.noreply.github.com>
2023-04-13 15:43:34 -0400

    This is the conflict challenge

--- GIT_exam_DSP_439.sh
+++ GIT_exam_DSP_439.sh
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-Hello, this is a change for data exam 3.
+Hello, this is a change for big data analysis exam 3.
 =======
 Hello, my name is Rafael, this is a  trial!!!!
 >>>>>>> ff49b5de164e28840f08d6c716deafeb674c5332
